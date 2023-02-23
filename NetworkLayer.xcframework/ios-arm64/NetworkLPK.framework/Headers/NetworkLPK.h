ff535d55b5119b00ea603b4dd83bfa477bf07239
Paresh Kanani <paresh.kanani@solutionanalysts.com>
2023-02-23 11:19:19 +0530

    add framework

---
+++ NetworkLayer.xcframework/ios-arm64/NetworkLPK.framework/Headers/NetworkLPK.h
@@ -0,0 +1,18 @@
+//
+//  NetworkLPK.h
+//  NetworkLPK
+//
+//  Created by Paresh Kanani on 23/02/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NetworkLPK.
+FOUNDATION_EXPORT double NetworkLPKVersionNumber;
+
+//! Project version string for NetworkLPK.
+FOUNDATION_EXPORT const unsigned char NetworkLPKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetworkLPK/PublicHeader.h>
+
+
